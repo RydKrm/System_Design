c4ce43df6c2ef7f2b8eba4c1c2c4570566384dad
Al Riyad Karim <124508907+RydKrm@users.noreply.github.com>
2025-07-03 09:20:26 +0600

    merge from Home

--- Golang/Code/09.Interfaces/interface2.go
+++ Golang/Code/09.Interfaces/interface2.go
@@ -1,40 +1,40 @@
-// package main
+package main
 
-// import "fmt"
+import "fmt"
 
-// type IMobile interface {
-// 	ring()
-// 	processor()
-// 	sendSMS(text string)
-// }
+type IMobile interface {
+	ring()
+	processor()
+	sendSMS(text string)
+}
 
-// type mobile struct {
-// 	name  string
-// 	model string
-// 	ram   uint16
-// }
+type mobile struct {
+	name  string
+	model string
+	ram   uint16
+}
 
-// func (mob mobile) ring() {
-// 	fmt.Println(mob.name + "ringing")
-// }
+func (mob mobile) ring() {
+	fmt.Println(mob.name + "ringing")
+}
 
-// func (mob mobile) sendSMS(text string){
-// 	fmt.Println(mob.name + "sending sms " + text)
-// }
+func (mob mobile) sendSMS(text string){
+	fmt.Println(mob.name + "sending sms " + text)
+}
 
-// func (mob mobile) processor(){
-// 	fmt.Println(mob.name + "with model " + mob.model + " is good" );
-// }
+func (mob mobile) processor(){
+	fmt.Println(mob.name + "with model " + mob.model + " is good" );
+}
 
-// func main() {
-// 	phone1 := mobile{
-// 		name:  "iphone",
-// 		model: "13s",
-// 		ram:   8,
-// 	}
+func main() {
+	phone1 := mobile{
+		name:  "iphone",
+		model: "13s",
+		ram:   8,
+	}
 
-// 	phone1.ring();
-// 	phone1.processor();
-// 	phone1.sendSMS("my name");
+	phone1.ring();
+	phone1.processor();
+	phone1.sendSMS("my name");
 
-// }
+}
