5348c73c36922e734114afe29205a188f34e52a2
RydKrm <alriyadkarim@gmail.com>
2025-10-21 23:58:54 +0600

    update(rust) : project running

--- Language/Rust/Code/src/main.rs
+++ Language/Rust/Code/src/main.rs
@@ -1,4 +1,4 @@
-use crate::structs::structs::Transaction;
+use crate::structs::structs::{BankAccount, Transaction};
 
 
 mod structs;
@@ -7,9 +7,25 @@ mod enums;
 
 fn main() {
     println!("Hello, world!");
-    let mut riyad_account = Transaction::new(
-        "Riyad_Karim".to_string(),
-        "RYD_12312".to_string()
+    let mut riyad_account = BankAccount::new(
+        "RYD_12312".to_string(),
+        "Riyad_Karim".to_string()
     );
+
+    let mut karim_account = BankAccount::new(
+        "krm_10010".to_string(),
+        "karim".to_string()
+    );
+
+    riyad_account.get_current_balance();
+
+    riyad_account.make_deposit(10000.0, "Salary".to_string());
+
+    karim_account.make_deposit(15000.0, "october salary".to_string());
+
+    println!("current amount ----->>>>>>>>>> {}", riyad_account.get_current_balance());
+
+
+
 }
 
