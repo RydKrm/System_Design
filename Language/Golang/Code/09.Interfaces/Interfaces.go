d0c6b0dbf29cae0c17c52ec26a8c5876504f16c9
RydKrm <alriyadkarim@gmail.com>
2025-12-27 23:33:26 +0600

    add(Golang) : Golang channel and advance topic added

--- Language/Golang/Code/09.Interfaces/Interfaces.go
+++ Language/Golang/Code/09.Interfaces/Interfaces.go
@@ -3,59 +3,59 @@ package interfaces
 import "fmt"
 
 type Developer struct {
-	name string
-	age int
-	job string
+	name                  string
+	age                   int
+	job                   string
 	competitiveProgrammer bool
-	exp int
-	language string 
+	exp                   int
+	language              string
 }
 
-type DeveloperInterface interface{
+type DeveloperInterface interface {
 	ageOk(expAge int) bool
-	isDeveloper()bool
+	isDeveloper() bool
 	isExperenced() bool
 	whatLanguage() string
 }
 
-func (d Developer) ageOk(expAge int) bool{
-   if d.age>= expAge {
-	return true
-   } else {
-	return false
-   }
-}
-
-func (d Developer) isDeveloper(currentJob string) bool{
-	if currentJob == d.job {
-		return true 
-	} else {
-		return false
-	}
-}
-
-func (d Developer) isExperenced(currentJobExp int) bool {
-	if currentJobExp>= d.exp {
+func (d Developer) ageOk(expAge int) bool {
+	if d.age >= expAge {
 		return true
 	} else {
 		return false
 	}
 }
 
-func (d Developer) whatLanguage() string{
+func (d Developer) isDeveloper(currentJob string) bool {
+	if currentJob == d.job {
+		return true
+	} else {
+		return false
+	}
+}
+
+func (d Developer) isExperenced(currentJobExp int) bool {
+	if currentJobExp >= d.exp {
+		return true
+	} else {
+		return false
+	}
+}
+
+func (d Developer) whatLanguage() string {
 	return d.language
 }
 
-func main(){
+func main2() {
 
-	developer1 := Developer {
-		name: "Riyadh",
-	    job: "backendDeveloper",
-	    age: 26,
+	developer1 := Developer{
+		name:                  "Riyadh",
+		job:                   "backendDeveloper",
+		age:                   26,
 		competitiveProgrammer: true,
-		exp:1,
-		language: "go",
-	 }
+		exp:                   1,
+		language:              "go",
+	}
 
 	if developer1.ageOk(30) {
 		fmt.Println("Developer age okay")
@@ -65,11 +65,11 @@ func main(){
 		fmt.Println("this is a developer ")
 	}
 
-	if developer1.isExperenced(2){
+	if developer1.isExperenced(2) {
 		fmt.Println("Developer has exprence ")
 	}
 
-    fmt.Println(developer1.whatLanguage())
+	fmt.Println(developer1.whatLanguage())
 
 	Geometry()
 
